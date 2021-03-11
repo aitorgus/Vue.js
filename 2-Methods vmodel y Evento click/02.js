a3354394efc9285f88fea1cbbeeefe4976829bec
AGCG1991 <aitor_gustavo@hotmail.com>
2021-03-11 23:56:10 +0100

    Metodos y evento click

---
+++ 2-Methods vmodel y Evento click/02.js
@@ -0,0 +1,26 @@
+//1º debemos de instanciar vue.js
+
+const app = new Vue({
+    //Aquí dentro trabajaremos con Vue
+    el: '#app', //le estamos diciendo dentro del contenedor app será gestionado con vue
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: [
+
+            { nombre: 'Pera', cantidad: 10 },
+            { nombre: 'Manzana', cantidad: 0 },
+            { nombre: 'Platano', cantidad: 11 }
+
+        ],
+        nuevaFruta: '' //Está vacio por defecto, en él insertaremos las distintas frutas que luego se añadirán al array
+    },
+    //Usamos la palabra reservada "methods" aquí irán declarado todos los métodos
+    methods: {
+        agregarFruta() {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0 //En este caso el valor de cantidad será estático
+            }) //Accedemos al array Frutas dentro del data y le insertamos valores
+        },
+
+    }
+})
